4940b7da48153c36981223cbc72a59a605d95fe1
Adrian Cardenas <arcardenas@gmail.com>
2013-07-14 12:33:41 -0500

    Fixed some bugs on displaying labels

--- src/ArcFormViewHelpers/View/Helper/Bootstrap/ArcControlGroup.php
+++ src/ArcFormViewHelpers/View/Helper/Bootstrap/ArcControlGroup.php
@@ -9,6 +9,7 @@ namespace ArcFormViewHelpers\View\Helper\Bootstrap;
 
 
 use Zend\Form\Element;
+use Zend\Form\View\Helper\FormLabel;
 use Zend\View\Helper\AbstractHelper;
 use Zend\View\Model\ViewModel;
 
@@ -56,7 +57,7 @@ class ArcControlGroup extends AbstractHelper
         $helper = $this->getViewHelper($element, $typeOverride);
 
         //These elements don't have a control group, just return their renders
-        if($element instanceof Element\Hidden || $element instanceof Element\Csrf || $element instanceof Element\Collection) {
+        if ($element instanceof Element\Hidden || $element instanceof Element\Csrf || $element instanceof Element\Collection) {
             return $helper->render($element);
         }
 
@@ -66,19 +67,24 @@ class ArcControlGroup extends AbstractHelper
         //Prepare the label mark up by checking if the class attribute
         //is defined & if it contains the control-label class if it's not a radio or checkbox
         $labelAttributes = $element->getLabelAttributes();
-        if (!array_key_exists('class', $labelAttributes)) {
+        if (!$labelAttributes || !array_key_exists('class', $labelAttributes)) {
             $labelAttributes['class'] = '';
         }
         //Only add the class to the label if it is one of the control elements
         if (in_array(get_class($element), $this->control)) {
-            $labelAttributes['class'] .= strstr($labelAttributes['class'], 'control-label') === false ? ' control-label' : '';
+            $labelAttributes['class'] .= strstr(
+                $labelAttributes['class'],
+                'control-label'
+            ) === false ? ' control-label' : '';
             $element->setLabelAttributes($labelAttributes);
             $label = $this->getView()->formLabel($element);
             $elementHtml = $helper->render($element);
-        } else if(in_array(get_class($element), $this->checkbox)) {
-            $labelAttributes['class'] .= strstr($labelAttributes['class'], 'checkbox') === false ? ' checkbox' : '';
-            $element->setLabelAttributes($labelAttributes);
-            $elementHtml = $this->getView()->formLabel($element, $helper->render($element));
+        } else {
+            if (in_array(get_class($element), $this->checkbox)) {
+                $labelAttributes['class'] .= strstr($labelAttributes['class'], 'checkbox') === false ? ' checkbox' : '';
+                $element->setLabelAttributes($labelAttributes);
+                $elementHtml = $this->getView()->formLabel($element, $helper->render($element), FormLabel::APPEND);
+            }
         }
 
         $decorator = new ViewModel(array('label' => $label, 'element' => $elementHtml));
@@ -95,12 +101,12 @@ class ArcControlGroup extends AbstractHelper
     protected function getViewHelper(Element $element, $typeOverride = '')
     {
         $class = '\Zend\Form\View\Helper\Form';
-        if(isset($typeOverride) && !empty($typeOverride)) {
+        if (isset($typeOverride) && !empty($typeOverride)) {
             $class .= $typeOverride;
         } else {
             $class .= array_pop(explode('\\', get_class($element)));
         }
-        if(class_exists($class, true)) {
+        if (class_exists($class, true)) {
             $helper = new $class;
         } else {
             $helper = new \Zend\Form\View\Helper\FormText();
